9e9135b8d8c12f34b89739249e25bacc35252469
Ataul <ataulnoor75@gmail.com>
2018-10-16 16:23:00 -0400

    working on proj

--- TeamApp/Form1.vb
+++ TeamApp/Form1.vb
@@ -1,4 +1,27 @@
 ﻿Public Class Form1
+    Public Sub RefreshLabel()
+        Label1.Text = NumInt
+    End Sub
+    Dim NumInt = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        NumInt = NumInt + 1
+        RefreshLabel()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        NumInt = NumInt - 1
+        RefreshLabel()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        NumInt = NumInt * 2
+        RefreshLabel()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        NumInt = NumInt / 2
+        RefreshLabel()
+    End Sub
     'Sami
 
 
