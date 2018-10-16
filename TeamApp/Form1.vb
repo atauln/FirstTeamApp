3c6134cf23bac483c2e8b3776fd85a4183ca1189
NoorA23 <NoorA23@FLMS-X015893>
2018-10-16 14:58:36 -0400

    novars

--- TeamApp/Form1.vb
+++ TeamApp/Form1.vb
@@ -1,9 +1,5 @@
 ﻿Public Class Form1
     'Sami
-    Dim garden = 15
-    Dim kelton = 19
-    Dim richard
-    Dim test = 0
 
 
 
@@ -15,9 +11,7 @@
 
     ' Ends Here
     'Ata
-    Dim Atasfakevar = -3
-    Dim boi = 1
-    Dim broimabackurminecraftacount = 100000000000000000000000000
+
 
 
 
@@ -29,7 +23,7 @@
 
     'Ends Here
     'Kelton
-    Dim Kelton1 = -0
+
 
 
 
